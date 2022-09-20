59fea344e321efc1de11937bc0c7e26ecd16ed90
Olawale Ayejuyone <olawaleayejuyone@gmail.com>
2022-09-20 01:47:17 -0400

    Test font on home page text

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -5,24 +5,24 @@ import "./index.scss";
 function Home() {
   return (
     <div className="home">
-      <div className="container">
-        <Row>
-          <Col>
-            <img
-              src={hookahVector}
-              width="100%"
-              height="100%"
-              alt="hookah vector"
-            />
-          </Col>
-          <h2 className="tuzoText">TUZO HOOKAH</h2>
-        </Row>
-        <div className="onePull">
-          <h3>1 pull</h3>
-          <h3>LOUD CLOUD</h3>
-        </div>
+      {/* <div className="container"> */}
+      <Row>
+        <Col>
+          <img
+            src={hookahVector}
+            width="100%"
+            height="100%"
+            alt="hookah vector"
+          />
+        </Col>
+      </Row>
+      <h2 className="tuzoText">TUZO HOOKAH</h2>
+      <div className="onePull">
+        <h3>1 pull</h3>
+        <h3>LOUD CLOUD</h3>
       </div>
     </div>
+    // </div>
   );
 }
 
